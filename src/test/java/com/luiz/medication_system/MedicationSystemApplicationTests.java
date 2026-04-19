ff74e60a3122b45f565d093194186f25210ebe0c
Luiz Eduardo Eleutério de Andrade <180665674+AnndradeLuiz@users.noreply.github.com>
2026-04-18 23:25:15 -0300

    Project Created

---
+++ src/test/java/com/luiz/medication_system/MedicationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luiz.medication_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
